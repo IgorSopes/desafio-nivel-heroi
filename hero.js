6af288887d9234b0f8603ce1ffc0d9c4c70eb8f2
IgorSopes <sopesigor@gmail.com>
2024-11-21 18:17:31 -0300

    teste do desafio

---
+++ hero.js
@@ -0,0 +1,32 @@
+console.log("Bem-vindo ao jogo!");
+
+let nomeHeroi = prompt("Digite seu nome:");
+let xpHeroi = parseInt(prompt("Digite sua quantidade de xp e descubra seu nível:"));
+
+let nivelHeroi;
+
+switch (true) {
+    case xpHeroi <= 1000:
+        nivel = "Ferro";
+        break;
+    case xpHeroi >= 1001 && xpHeroi <= 2000:
+        nivel = "Bronze";
+        break;
+    case xpHeroi >= 2001 && xpHeroi <= 5000:
+        nivel = "Prata";
+        break;
+    case xpHeroi >= 5001 && xpHeroi <= 7000:
+        nivel = "Ouro";
+        break;
+        case xpHeroi >= 7001 && xpHeroi <= 8000:
+        nivel = "Platina";
+        break;
+    case xpHeroi >= 8001 && xpHeroi <= 9000:
+        nivel = "Ascendente";
+        break;
+    case xpHeroi >= 9001 && xpHeroi <= 10000:
+        nivel = "Imortal";
+        break;
+    default:
+        nivel = "Radiante";
+    };
